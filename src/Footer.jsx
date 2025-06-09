0296d1003e3f21fe14b9249183bd12d3c2350cba
ChaitanyaDeepthi123 <chaitanyadeepthibandi@gmail.com>
2025-06-09 16:01:03 +0530

    updated

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div class="App-Footer">
+        <div>
+            <h3>Contact Details</h3>
+            <p>Mobile:8977902927
+                <br></br>
+                Email:deepthireddy1207@gmail.com
+            </p>
+        </div>
+    </div>
+  )
+}
+
